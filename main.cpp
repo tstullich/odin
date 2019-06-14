5f07b1f2fadda3dca5457103f56f8a02ac3ea064
Tim Stullich <tstullich@protonmail.com>
2019-06-14 21:40:29 +0200

    Added render pass configuration

--- main.cpp
+++ main.cpp
@@ -93,6 +93,7 @@ class HelloTriangleApplication {
   VkExtent2D swapChainExtent;
 
   VkPipelineLayout pipelineLayout;
+  VkRenderPass renderPass;
 
   void initWindow() {
     glfwInit();
@@ -123,6 +124,7 @@ class HelloTriangleApplication {
 
   void cleanup() {
     vkDestroyPipelineLayout(device, pipelineLayout, nullptr);
+    vkDestroyRenderPass(device, renderPass, nullptr);
 
     for (auto imageView : swapChainImageViews) {
       vkDestroyImageView(device, imageView, nullptr);
@@ -471,6 +473,38 @@ class HelloTriangleApplication {
     VkAttachmentDescription colorAttachment = {};
     colorAttachment.format = swapChainImageFormat;
     colorAttachment.samples = VK_SAMPLE_COUNT_1_BIT;
+    // Clear the framebuffer before and after rendering
+    colorAttachment.loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR;
+    // Store color data in memory for future read operations
+    colorAttachment.storeOp = VK_ATTACHMENT_STORE_OP_STORE;
+    // Configure if stencil data should be stored
+    colorAttachment.stencilLoadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE;
+    colorAttachment.stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
+    colorAttachment.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
+    colorAttachment.finalLayout = VK_IMAGE_LAYOUT_PRESENT_SRC_KHR;
+
+    // Configure subpasses. For now we just need the one for rendering
+    VkAttachmentReference colorAttachmentRef = {};
+    colorAttachmentRef.attachment = 0;
+    colorAttachmentRef.layout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL;
+
+    VkSubpassDescription subpass = {};
+    subpass.pipelineBindPoint = VK_PIPELINE_BIND_POINT_GRAPHICS;
+    subpass.colorAttachmentCount = 1;
+    // This can be directly referenced in our fragment shader
+    subpass.pColorAttachments = &colorAttachmentRef;
+
+    VkRenderPassCreateInfo renderPassInfo = {};
+    renderPassInfo.sType = VK_STRUCTURE_TYPE_RENDER_PASS_CREATE_INFO;
+    renderPassInfo.attachmentCount = 1;
+    renderPassInfo.pAttachments = &colorAttachment;
+    renderPassInfo.subpassCount = 1;
+    renderPassInfo.pSubpasses = &subpass;
+
+    if (vkCreateRenderPass(device, &renderPassInfo, nullptr, &renderPass) !=
+        VK_SUCCESS) {
+      throw std::runtime_error("Failed to create render pass!");
+    }
   }
 
   void createSwapChain() {
