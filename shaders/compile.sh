22d63c21e74f4351873bb184ced113be29ef8f8a
Tim Stullich <tstullich@protonmail.com>
2019-07-15 23:26:35 +0200

    Updated shader compile script to run during build

--- shaders/compile.sh
+++ shaders/compile.sh
@@ -1,2 +1,11 @@
+#!/usr/bin/bash
+
 # Compile all shaders with the following extensions
-glslangValidator -V *.frag *.vert
+VALIDATOR_PATH="$(command -v glslangValidator)"
+if ! [ $VALIDATOR_PATH ]; then
+  echo 'Error: glslangValidator is not installed.
+       Make sure you have the Vulkan SDK installed and in your PATH variable' >&2
+  exit 1
+fi
+
+$VALIDATOR_PATH -V *.frag *.vert
